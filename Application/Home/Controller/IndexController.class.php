b8e3e2b8f55c9a923093fad4576b9d0ad2650bcf
doclu <docluwang@163.com>
2018-09-03 17:20:22 +0800

    pp

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,37 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+        
+        //采集首页地址
+        $url="http://www.hznzcn.com/product-670922.html";
+        //获取页面代码
+        $rs=file_get_contents($url);
+        var_dump($rs);
+        //echo $rs;die;
+        //设置匹配正则
+        //$fp=fopen("text.txt","a");
+        //$fw=fwrite($fp,$rs);
+        //fclose($fp);
+        /*<I class=titles><A
+        href="http://www.xz-src.com/"
+        target=_blank>留住你身边的好男人</A></I>*/
+        //$preg='/<i\s+class=\"titles\"><a\s+href=\"[^>]+\">(.*)<\/a><\/i>/i';
+        $preg_title='/<h1>(.+?)<\/h1>/i';
+        $preg_price='/<label id=\"productShopPrice\">(.+?)<\/label>/';
+        $preg_img='/<img id=\"midimg\"+.+src=\"(.+?)\"+.+>/';
+        //进行正则搜索/<label id=\"productShopPrice\">(.+?)</label>/
+        preg_match_all($preg_title,$rs,$title);
+        preg_match_all($preg_price,$rs,$price);
+        preg_match_all($preg_img,$rs,$img);
+        //计算标题数量
+        var_dump($title);
+      echo $title[0][1];
+        echo $price[0][1];
+       echo $img[0][1];
+        
+        $a['title']=$title[1][0];
+        $a['price']=$price[1][0];
+        $a['url']=$img[1][0];
+        echo json_encode($a,JSON_UNESCAPED_UNICODE);die;
     }
 }
